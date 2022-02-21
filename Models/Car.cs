6b3ef35c449a1a40b1483ecfa0d26cc95c8934a5
Ryan Bass <theryanbass@gmail.com>
2022-02-21 09:49:57 -0800

    add getter methods and private constructor
    Co-Authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- Models/Car.cs
+++ Models/Car.cs
@@ -2,20 +2,34 @@ namespace Dealership.Models
 {
   public class Car
   {
-    public string MakeModel;
-    public int Price;
-    public int Miles;
+    private string _makeModel;
+    private int _price;
+    private int _miles;
 
     public Car(string makeModel, int price, int miles)
     {
-      MakeModel = makeModel;
-      Price = price;
-      Miles = miles;
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+    public string GetMakeModel()
+    {
+      return _makeModel;
     }
 
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+    
     public bool WorthBuying(int maxPrice)
     {
-      return (Price <= maxPrice);
+      return (_price <= maxPrice);
     }
   }
 }
